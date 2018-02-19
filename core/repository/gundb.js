21b6947e76e1892c55938440b7dd2e346e555a8d
DESKTOP-T3E05UA\pablo bianco <pablo.m.bianco@hotmail.com>
2018-02-18 21:55:09 -0300

    Gun db in progress

--- core/repository/gundb.js
+++ core/repository/gundb.js
@@ -8,7 +8,26 @@ module.exports.db = function(http){
     var gun = Gun(gunOptions);
 
     var Suscribe = function(key, callback){
-        
+        gun.get(key).on(function(data, _key){
+            var puntero = data._['>'];
+            var keyNames = Object.keys(puntero);
+            update.push(keyNames[0]);
+            gun.get(keyNames[0]).val(function(data){
+                var obj = {
+                    dbId: data._['#'],
+                    magnet: data.magnet,
+                    title: data.title,
+                    imgLink: data.imgLink,
+                    description: data.description,
+                    timestamp: data.timestamp,
+                };
+                io.sockets.emit('new', obj);
+                latestUploads.push(obj);
+                if(latestUploads.length > 20){
+                    latestUploads.shift();
+                }
+            });
+        },true);
     }
 
     var Add = function(key, object, callback){
