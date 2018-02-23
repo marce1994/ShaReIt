1d39b19fabdee28ba12d8ee567a13dba1f2754b4
DESKTOP-T3E05UA\pablo bianco <pablo.m.bianco@hotmail.com>
2018-02-23 00:06:29 -0300

    m

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ io.on('connection', function(client){
     });
 });
 
-if (cluster.isMaster) {
+/*if (cluster.isMaster) {
     console.log(`Master ${process.pid} is running`);
     // Fork workers.
     for (let i = 0; i < numCPUs; i++) {
@@ -39,9 +39,9 @@ if (cluster.isMaster) {
     cluster.on('exit', (worker, code, signal) => {
         console.log(`worker ${worker.process.pid} died`);
     });
-} else {
+} else {*/
     //Application run
     var port = process.env.PORT || 8080;
     http.listen(port);
     console.log(`Worker ${process.pid} started and listening on port ${port}`);
-}
+//}
