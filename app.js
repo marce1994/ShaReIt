1a9c14cb61be2b4c13b5c753b174f33a19fadf04
DESKTOP-T3E05UA\pablo bianco <pablo.m.bianco@hotmail.com>
2018-02-26 00:55:16 -0300

    tests steemit

--- app.js
+++ app.js
@@ -6,6 +6,68 @@ const numCPUs = require('os').cpus().length;
 var express = require("express");
 var app = express();
 
+//steemit
+var steem = require('steem');
+
+/*var isValidUsername = steem.utils.validateAccountName('a1');
+console.log(isValidUsername);*/
+
+//votar
+/*steem.broadcast.vote(wif, voter, author, permlink, weight, function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getTrendingTags('anime', 10, function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getRecentCategories('anime', 10, function(err, result) {
+    console.log(err, result);
+  });*/
+
+  /*steem.api.getConfig(function(err, result) {
+    console.log(err, result);
+  });*/
+
+  /*steem.api.getCurrentMedianHistoryPrice(function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getHardforkVersion(function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getNextScheduledHardfork(function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.login('', '', function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getAccounts(["pablobianco"], function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getAccountCount(function(err, result) {
+    console.log(err, result);
+  });*/
+  /*steem.api.getOrderBook(100, function(err, result) {
+    console.log(err, result);
+  });*/
+
+  //Obtiene los votos del post.
+  /*steem.api.getActiveVotes("pablobianco", "shareit-network", function(err, result) {
+    console.log(err, result);
+  });*/
+  //Obtiene los votod realizados por el usuario.
+  /*steem.api.getAccountVotes("pablobianco", function(err, result) {
+    console.log(err, result);
+  });*/
+  //Obtiene el contenido del post, titulo y practicamente todo.
+  /*steem.api.getContent("pablobianco", "shareit-network", function(err, result) {
+    console.log(err, result);
+  });*/
+
+
+  /*steem.api.getWitnessByAccount("pablobianco", function(err, result) {
+    console.log(err, result);
+  });*/
+//steemit end...
+
 var webrouter = require('./web/router.js');
 app.use(webrouter);
 
@@ -21,9 +83,10 @@ io.on('connection', function(client){
 
     database.ListObj({}, function(docs){
         console.log(docs.length);
-        docs.forEach(element => {
-            client.emit('new',element);
-        });
+        for (var i = docs.length - 1; i >= 0; i--)
+        {
+            client.emit('new',docs[i]);
+        }
     });
 
     client.on('message', function(data)
@@ -33,6 +96,7 @@ io.on('connection', function(client){
 
     client.on('add', function(data)
     {
+        console.log(data);
         database.AddObj(data);
     });
 });
